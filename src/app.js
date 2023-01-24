95780a9f41b9decd315660b00b7b57d7ddd1f1cc
Blancreyes <luijbr83@gmail.com>
2023-01-24 08:26:24 +0000

    Project Finished

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("domain").innerHTML = domainGenerator();
 };
+
+function domainGenerator() {
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+
+  let i = Math.floor(Math.random() * pronoun.length);
+  let j = Math.floor(Math.random() * adj.length);
+  let k = Math.floor(Math.random() * noun.length);
+
+  return pronoun[i] + adj[j] + noun[k] + ".com";
+}
+
+console.log(domainGenerator);
